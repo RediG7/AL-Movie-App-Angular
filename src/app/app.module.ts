1c58e64a5f049244e593d097e980eb20a0d632a4
RediG7 <guleksiredi7@gmail.com>
2022-06-01 00:46:57 +0200

    Items Banner Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { SliderComponent } from './components/slider/slider.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     MoviesComponent,
     SliderComponent,
+    ItemsBannerComponent,
   ],
   imports: [
     BrowserModule,
